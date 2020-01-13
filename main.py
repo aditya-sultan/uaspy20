8014d3c60ab6aafa5baa838a3698fd14cf6fd160
Sultanaditia53@gmail.com <sultanaditia53@gmail.com>
2020-01-13 11:52:30 +0700

    uaspy20

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from core.baseapp import BaseApp
+from view.view_book import *
+
 class MainApp():
     def __init__(self):
         self.books = []
+
+    def list_book(self):
+        self.list_books()
 
 
 if __name__ == "__main__":
